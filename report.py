72bfec22e97629cd88ea561f53a8231a1b376c7a
Nisha K <nishak@vmware.com>
2017-08-14 09:48:04 -0700

    Added tern executable
    Signed-off-by: Nisha K <nishak@vmware.com>

--- report.py
+++ report.py
@@ -3,32 +3,32 @@ Edit a report string and write it to a file
 '''
 
 # constants
-report_file = 'sources.txt'
-report_recog = 'Recognized sources:\n'
+report_file = 'report.txt'
+report_confirmed = 'Confirmed sources:\n'
+report_unconfirmed = 'Unconfirmed sources:\n'
 report_package = '\t{package_name}\n'
 report_url = '\t\turl: {url}\n'
 report_version = '\t\tversion: {version}\n'
-report_unrecog = 'Unrecognized sources:\n'
+report_unrecog = 'Unrecognized packages:\n'
 
 
 def record_report(report_dict):
     '''The report dict will look like this:
-        recognized:
-            packages:[{name: <name>, url: <url>, version: <version>}...]
-        unrecognized:
-            packages:[<package names>]
+        confirmed: [{name: <name>, url: <url>, version: <version>}...]
+        unconfirmed:[{name: <name>, url: <url>, version: <version>}...]
+        unrecognized: [<package names>]
     Record the report with each of these values'''
-    report = report_recog
-    for package in report_dict['recognized']['packages']:
+    report = report_confirmed
+    for package in report_dict['confirmed']:
         report = report + report_package.format(package_name=package['name'])
-        if 'url' in package.keys():
-            report = report + report_url.format(url=package['url'])
-        if 'version' in package.keys():
-            report = report + report_version.format(
-                version=package['version'])
-    report = report + report_unrecog
-    for package in report_dict['unrecognized']['packages']:
-        report = report + package + ' '
+        report = report + report_url.format(url=package['url'])
+        report = report + report_version.format(version=package['version'])
+    report = report + report_unconfirmed
+    for package in report_dict['unconfirmed']:
+        report = report + report_package.format(package_name=package['name'])
+        report = report + report_url.format(url=package['url'])
+        report = report + report_version.format(version=package['version'])
+    report = report + package + ' '
     return report
 
 
