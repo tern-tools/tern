77401b816cfe8af99a2c83417622b932190f2eca
Nisha K <nishak@vmware.com>
2017-12-19 09:48:53 -0800

    Refactor: Added Image class
    Image contains all the data and methods associated with a container
    image. The method load_image will use utils/metadata.py to get
    all the metadata from the image. Some of the modules used from
    commands and metadata are incompatible with the state of commands
    and metadata right now so this will break.
    
    This is commit 2 in the set of refactor commits to move modules
    from utils to class methods
    
    Signed-off-by: Nisha K <nishak@vmware.com>

---
+++ classes/image.py
@@ -0,0 +1,98 @@
+'''
+Copyright (c) 2017 VMware, Inc. All Rights Reserved.
+SPDX-License-Identifier: BSD-2-Clause
+'''
+
+from utils import commands as cmds
+from utils import metadata as md
+from .image_layer import ImageLayer
+
+
+class Image(object):
+    '''A representation of the image to be analyzed
+    attributes:
+        repotag: the repotag used to reference the image
+        id: this is a unique identifier for the image - for OCI spec this could
+        be the digest. For now this is the sha256sum of the config.json in a
+        Docker compatible manifest
+        manifest: the json object representing the image manifest
+        repotags: the list of repotags associated with this image
+        config: the image config metadata
+        layers: list of layer objects in the image
+        history: a list of commands used to create the filesystem layers
+        This is used in Docker images to match Dockerfiles but is an optional
+        part of OCI compatible images
+    methods:
+        load_image: this method uses the metadata utility to load all
+        the metadata for the image
+    '''
+    def __init__(self, repotag=None, id=None):
+        '''Either initialize using the repotag or the id'''
+        self.__repotag = repotag
+        self.__id = id
+        self.__manifest = {}
+        self.__repotags = []
+        self.__config = {}
+        self.__layers = []
+        self.__history = None
+
+    @property
+    def repotag(self):
+        return self.__repotag
+
+    @property
+    def manifest(self):
+        return self.__manifest
+
+    @property
+    def id(self):
+        return self.__id
+
+    @property
+    def repotags(self):
+        return self.__repotags
+
+    @property
+    def config(self):
+        return self.__config
+
+    @property
+    def layers(self):
+        return self.__layers
+
+    def get_image_option(self):
+        '''Check to see which value was used to init the image object
+        Return the value that was used. If neither one was used raise
+        NameError. If both were used return the id'''
+        if self.repotag is not None and self.id is not None:
+            return self.id
+        elif self.repotag is not None:
+            return self.repotag
+        elif self.id is not None:
+            return self.id
+        else:
+            raise NameError("Image object initialized with no repotag or ID")
+
+    def load_image(self):
+        '''Load image metadata based on the image build tool
+        There is no standard format and there are several tools used to build
+        container images. For now only docker save is supported
+        '''
+        try:
+            option = self.get_image_option()
+            if cmds.extract_image_metadata(option):
+                print('Image extracted')
+            else:
+                print('Failed to extract image')
+            self.__manifest = md.get_image_manifest()
+            self.__id = md.get_image_id(self.__manifest)
+            self.__repotags = md.get_image_repotags(self.__manifest)
+            self.__config = md.get_image_config(self.__manifest)
+            layer_paths = md.get_image_layers(self.__manifest)
+            layer_diffs = md.get_diff_ids(self.__config)
+            while layer_diffs and layer_paths:
+                layer = ImageLayer(layer_diffs.pop(0), layer_paths.pop(0))
+                self.__layers.append(layer)
+        except NameError as error:
+            print(error)
+            raise NameError(error)
