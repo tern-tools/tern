7185c5c2ea08059ca4493269f92775e3625b5757
Nisha K <nishak@vmware.com>
2017-12-20 06:21:29 -0800

    Refactor: Added unit tests for Image class
    This is commit 4 of the refactor to move modules from utils
    to classes
    
    - Fixed the unit test for ImageLayer class
    - Fixed the unit test for Package class
    - Renamed a module in metadata
    
    All the unit tests for the classes will pass but the tests for
    the utilities will fail
    
    Signed-off-by: Nisha K <nishak@vmware.com>

--- tests/test_class_package.py
+++ tests/test_class_package.py
@@ -18,35 +18,35 @@ class TestClassPackage(unittest.TestCase):
 
     def testInstance(self):
         self.assertEqual(self.package.name, 'x')
-        self.assertEqual(self.package.version, 0.0)
+        self.assertEqual(self.package.version, '')
         self.assertFalse(self.package.src_url)
         self.assertFalse(self.package.license)
 
     def testSetters(self):
         self.assertRaises(AttributeError, setattr, self.package, 'name', 'y')
-        self.package.version = 1.0
-        self.assertEqual(self.package.version, 1.0)
+        self.package.version = '1.0'
+        self.assertEqual(self.package.version, '1.0')
         self.package.license = 'Apache 2.0'
         self.assertEqual(self.package.license, 'Apache 2.0')
         self.package.src_url = 'github.com'
         self.assertEqual(self.package.src_url, 'github.com')
 
     def testGetters(self):
-        self.package.version = 1.0
+        self.package.version = '1.0'
         self.package.license = 'Apache 2.0'
         self.package.src_url = 'github.com'
         self.assertEqual(self.package.name, 'x')
-        self.assertEqual(self.package.version, 1.0)
+        self.assertEqual(self.package.version, '1.0')
         self.assertEqual(self.package.license, 'Apache 2.0')
         self.assertEqual(self.package.src_url, 'github.com')
 
     def testToDict(self):
-        self.package.version = 1.0
+        self.package.version = '1.0'
         self.package.license = 'Apache 2.0'
         self.package.src_url = 'github.com'
         a_dict = self.package.to_dict()
         self.assertEqual(a_dict['name'], 'x')
-        self.assertEqual(a_dict['version'], 1.0)
+        self.assertEqual(a_dict['version'], '1.0')
         self.assertEqual(a_dict['license'], 'Apache 2.0')
         self.assertEqual(a_dict['src_url'], 'github.com')
 
