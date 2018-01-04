e184f20dc12d50fa8949b24d87c6fc63aec6622f
Nisha K <nishak@vmware.com>
2018-01-03 17:17:38 -0800

    Refactor: Added new class: Command
    This is commit 5 of the refactor to move modules in utils into
    classes
    
    Command represents a shell command or an instruction in a shell
    script typically of the form:
    
    command [subcommand] [options] [arguments]
    
    which loosely follows the POSIX command line interface standard
    
    The parser itself resides in utils/general.py but the object
    to encapsulate all the pieces and the operations on them are now
    in this class.
    
    The tests specifically targets shell commands commonly used to
    install software components.
    
    Signed-off-by: Nisha K <nishak@vmware.com>

--- utils/general.py
+++ utils/general.py
@@ -18,35 +18,43 @@ def pushd(path):
 
 
 def parse_command(command):
-    '''Typically a unix command is of the form:
+    '''Parse a unix command of the form:
         command (subcommand) [options] [arguments]
+        Caveats:
+            1. There is no way of knowing whether a command contains
+            subcommands or not so those tokens will be identified as 'words'
+            2. There is no way of knowing whether an option requires an
+            argument or not. The arguments will be identified as 'words'
+        For most cases involving package management this should be enough to
+        identify whether a package was meant to be installed or removed.
     Convert a given command into a dictionary of the form:
         {'name': command,
-         'subcommand': subcommand,
-         'options': [list of options]
-         'arguments': [list of arguments]}'''
+         'options': [list of option tuples]
+         'words': [remaining words]}
+    An option tuple contains the option flag and the option argument
+    We look ahead to see if the token after the option flag does not match
+    the regex for an option flag and assumes that is the argument
+    The token still remains in the words list because we do not know for sure
+    if it is a command argument or an option argument'''
     options = re.compile('^-')
-    options_list = []
-    args_list = []
+    option_list = []
+    word_list = []
     command_dict = {}
-    command_words = command.split(' ')
-    # first word is the command name
-    command_dict.update({'name': command_words.pop(0), 'subcommand': ''})
-    # check if the first word is an option
-    first = command_words.pop(0)
-    if not options.match(first):
-        # this is a subcommand
-        command_dict.update({'subcommand': first})
-    else:
-        # append this to the list of options
-        options_list.append(first)
-    # find options and arguments in the rest of the list
-    while command_words:
-        if options.match(command_words[0]):
-            options_list.append(command_words.pop(0))
+    command_tokens = command.split(' ')
+    # first token is the command name
+    command_dict.update({'name': command_tokens.pop(0)})
+    # find options in the rest of the list
+    while command_tokens:
+        if options.match(command_tokens[0]):
+            option_flag = command_tokens.pop(0)
+            if not options.match(command_tokens[0]):
+                option_arg = command_tokens[0]
+            else:
+                option_arg = ''
+            option_list.append((option_flag, option_arg))
         else:
-            args_list.append(command_words.pop(0))
-    # now we have options and arguments
-    command_dict.update({'options': options_list,
-                         'arguments': args_list})
+            word_list.append(command_tokens.pop(0))
+    # now we have options and the remainder words
+    command_dict.update({'options': option_list,
+                         'words': word_list})
     return command_dict
