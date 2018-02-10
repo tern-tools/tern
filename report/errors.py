81ec61a49bedb47ae1200413f8696f075d6e5957
Nisha K <nishak@vmware.com>
2018-02-10 12:59:55 -0800

    Refactor: Added check for command library keys
    In order for the tool to be successful in retrieving package information
    certain retrieval method listings need to be present in the command
    library. Added a list of keys to command_lib.py that should be present.
    If they are not present, the tool will not fail but will give a warning
    message about adding the retrieval method.
    
    - Added base image keys and package keys immutable sets
    - Added a module to check for the keys
    - Added error messages to the errors template
    
    At this point the main executable does not work
    
    Signed-off-by: Nisha K <nishak@vmware.com>

--- report/errors.py
+++ report/errors.py
@@ -7,18 +7,24 @@ SPDX-License-Identifier: BSD-2-Clause
 Error messages
 '''
 
-no_packages = '''Unable to recover packages for layer {layer}.
-Consider either entering them manually or create a bash script to retrieve the
-package in the command library.\n'''
-no_version = '''No version for package {package}.
-Consider either entering the version manually or creating a script to retrieve
-it in the command library\n'''
-no_license = '''No license for package {package}.
-Consider either entering the license manually or creating a script to retrieve
-it in the command library\n'''
-no_src_url = '''No source url for package {package}.
-Consider either entering the source url manually or creating a script to
-retrieve it in the command library\n'''
-env_dep_dockerfile = '''Docker build failed: {build_fail_msg} \n
-Since the Docker image cannot be built, Tern will try to retrieve package
-information from the Dockerfile itself.\n'''
+no_packages = '''Unable to recover packages for layer {layer_id}. Consider ''' \
+    '''either entering them manually or create a bash script to retrieve ''' \
+    '''the package in the command library.\n'''
+no_version = '''No version for package {package_name}. Consider either ''' \
+    '''entering the version manually or creating a script to retrieve ''' \
+    '''it in the command library\n'''
+no_license = '''No license for package {package_name}. Consider either ''' \
+    '''entering the license manually or creating a script to retrieve it ''' \
+    '''in the command library\n'''
+no_src_url = '''No source url for package {package_name}. Consider either ''' \
+    '''entering the source url manually or creating a script to retrieve ''' \
+    '''it in the command library\n'''
+env_dep_dockerfile = '''Docker build failed: {build_fail_msg} \n Since ''' \
+    '''the Docker image cannot be built, Tern will try to retrieve ''' \
+    '''package information from the Dockerfile itself.\n'''
+no_listing_for_base_key = '''No listing for key {listing_key}. ''' \
+    '''Consider adding this listing to command_lib/base.yml.\n'''
+no_listing_for_package_key = '''No listing for key {listing_key}. ''' \
+    '''Consider adding this listing to command_lib/snippets.yml.\n'''
+unsupported_listing_for_key = '''Unsupported listing for key ''' \
+    '''{listing_key}.\n'''
