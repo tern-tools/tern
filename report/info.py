39f226845500b50533697d48c02e5e7f4c96218c
Nisha K <nishak@vmware.com>
2018-03-13 13:43:32 -0700

    Refactor: Added more messages
    - Added info.py for informational messages
    - Added more error messages
    
    The tern executable does not work
    
    Signed-off-by: Nisha K <nishak@vmware.com>

---
+++ report/info.py
@@ -0,0 +1,13 @@
+'''
+Copyright (c) 2017 VMware, Inc. All Rights Reserved.
+SPDX-License-Identifier: BSD-2-Clause
+'''
+
+'''
+Informational messages
+'''
+
+loading_from_cache = '''Loading packages from cache for layer {layer_id}:'''
+invoking_base_commands = '''Invoking commands from command_lib/base.yml:'''
+invoking_snippet_commands = '''Invoking commands from ''' \
+    '''command_lib/snippets.yml'''
