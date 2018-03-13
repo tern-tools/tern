d681686957b6bd76dd0203a2aefd355a8b9c3a15
Nisha K <nishak@vmware.com>
2018-03-13 13:45:08 -0700

    Refactor: Separated Docker specific modules
    Separated the Docker specific modules from modules
    that can be used for all container images. There will
    probably be more refactors to separate common tern modules based
    on how many types of container images that will be supported
    
    The tern executable does not work
    
    Signed-off-by: Nisha K <nishak@vmware.com>

--- common.py
+++ common.py
@@ -9,9 +9,14 @@ import sys
 
 from classes.layer import Layer
 from classes.package import Package
-from command_lib import command_lib as cmds
+from classes.notice import Notice
+from command_lib import command_lib as cmdlib
+from report import info
+from report import errors
 from utils import cache as cache
 from utils import metadata as meta
+from utils import constants as const
+from utils.container import check_container
 '''
 Common functions
 '''
@@ -20,70 +25,91 @@ Common functions
 logger = logging.getLogger('ternlog')
 
 
-def get_image_shell(base_image_tag):
-    '''Given the base image tag tuple
-    (<image>, <tag>) look up the base library for the shell'''
-    return cmds.command_lib[
-        'base'][base_image_tag[0]]['tags'][base_image_tag[1]]['shell']
+def load_from_cache(image):
+    '''Given an image object, check against cache to see if a layer id exists
+    if yes then get the package list and load it in the image layer. If it
+    doesn't exist continue. If not all the layers have packages, return False
+    else return True'''
+    is_full = True
+    for layer in image.layers:
+        if not layer.packages:
+            raw_pkg_list = cache.get_packages(layer.id)
+            if not raw_pkg_list:
+                is_full = False
+            else:
+                from_cache_notice = Notice()
+                from_cache_notice.origin = image.get_image_option() + \
+                    layer.id
+                from_cache_notice.message = info.loading_from_cache.format(
+                    layer_id=layer.id)
+                from_cache_notice.level = 'info'
+                layer.add_notice(from_cache_notice)
+                for pkg_dict in raw_pkg_list:
+                    pkg = Package(pkg_dict['name'])
+                    pkg.fill(pkg_dict)
+                    layer.add_package(pkg)
+    return image, is_full
 
 
-def get_base_packages(base_image_tuple, info):
-    '''Given a base image and tag tuple, get a list of package objects from
+def add_base_packages(image, info):
+    '''Given an image object, get a list of package objects from
     invoking the commands in the command library base section:
         1. For the image and tag name find if there is a list of package names
         2. If there is an invoke dictionary, invoke the commands
-        3. Create a list of packages'''
-    pkg_list = []
+        3. Create a list of packages
+        4. Add them to the image'''
     # information under the base image tag in the command library
-    listing = cmds.get_base_listing(base_image_tuple[0])
+    listing = cmdlib.get_base_listing(image.name, image.tag)
+    origin = 'command_lib/base.yml'
+    pkg_list = []
     if listing:
-        names = cmds.get_pkg_attr_list(info['shell'], info['names'])
-        versions = cmds.get_pkg_attr_list(info['shell'], info['versions'])
-        licenses = cmds.get_pkg_attr_list(info['shell'], info['licenses'])
-        src_urls = cmds.get_pkg_attr_list(info['shell'], info['src_urls'])
-        if names and len(names) > 1:
-            for index in range(0, len(names)):
-                pkg = Package(names[index])
-                if len(versions) == len(names):
-                    pkg.version = versions[index]
-                if len(licenses) == len(names):
-                    pkg.license = licenses[index]
-                if len(src_urls) == len(names):
-                    pkg.src_url = src_urls[index]
-                pkg_list.append(pkg)
+        shell, msg = cmdlib.get_image_shell(listing)
+        if not shell:
+            # add a warning notice for no shell in the command library
+            no_shell_message = errors.no_shell_listing.format(
+                image_name=image.name, image_tag=image.tag,
+                default_shell=const.shell)
+            no_shell_notice = Notice(origin, no_shell_message, 'warning')
+            image.add_notice(no_shell_notice)
+            # add a hint notice to add the shell to the command library
+            add_shell_message = errors.no_listing_for_base_key.format(
+                listing_key='shell')
+            add_shell_notice = Notice(origin, add_shell_message, 'hint')
+            image.add_notice(add_shell_notice)
+            shell = const.shell
+        # check if a container is running first
+        # eventually this needs to change to use derivatives that have
+        # more than 1 layer
+        # for now, we add the list of packages to all the layers in a
+        # starting base image
+        if check_container():
+            names, n_msg = cmdlib.get_pkg_attr_list(shell, info['names'])
+            versions, v_msg = cmdlib.get_pkg_attr_list(shell, info['versions'])
+            licenses, l_msg = cmdlib.get_pkg_attr_list(shell, info['licenses'])
+            src_urls, u_msg = cmdlib.get_pkg_attr_list(shell, info['src_urls'])
+            # add a notice to the image if something went wrong
+            invoke_msg = n_msg + v_msg + l_msg + u_msg
+            if invoke_msg:
+                pkg_invoke_notice = Notice(origin, invoke_msg, 'error')
+                image.add_notice(pkg_invoke_notice)
+            if names and len(names) > 1:
+                for index in range(0, len(names)):
+                    pkg = Package(names[index])
+                    if len(versions) == len(names):
+                        pkg.version = versions[index]
+                    if len(licenses) == len(names):
+                        pkg.license = licenses[index]
+                    if len(src_urls) == len(names):
+                        pkg.src_url = src_urls[index]
+                    pkg_list.append(pkg)
+        # if no container is running give a logging error
         else:
-            logger.warning(cannot_retrieve_base_packages.format(
-                image=base_image_tag[0], tag=base_image_tag[1]))
+            logger.error(errors.no_running_docker_container)
+    # if there is no listing add a notice
     else:
-        logger.warning(no_image_tag_listing.format(
-            image=base_image_tag[0], tag=base_image_tag[1]))
-    return pkg_list
+        no_listing_notice = Notice(origin, no_image_tag_listing.format(
+            image_name=image.name, image_tag=image.tag), 'error')
 
-
-def get_base_obj(base_image_tag):
-    '''Get a list of layers with their associated packages:
-        1. Check if the base image exists on the host machine
-        2. Get the layers
-        3. For each layer check if there is a list of packages associated
-        with it in the cache
-        4. If there are no packages return a list of empty layer objects'''
-    obj_list = []
-    if not check_base_image(base_image_tag):
-        # the base image cannot be found locally nor remotely
-        # at this point there is no context for Tern to use so raise an
-        # exception to exit gracefully
-        logger.error(base_image_not_found)
-        sys.exit(1)
-    else:
-        cache.load()
-        # get the history with diff ids
-        # tuples look like this: (history command, diff_id)
-        layer_history = get_layer_history(get_image_tag_string(
-            base_image_tag))
-        for layer_tuple in layer_history:
-            layer_obj = get_layer_obj(layer_tuple[1])
-        obj_list.append(layer_obj)
-    return obj_list
 
 
 def get_layer_obj(diff_id):
