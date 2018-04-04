c06e4e24de23c2f0d9e12ff3507969efc7a1e010
Nisha K <nishak@vmware.com>
2018-04-04 09:08:33 -0700

    Refactor: WIP: create a report
    This is incomplete work
    
    - execute_dockerfile is completed uptil functionality to
    analyze a Docker image based on history. It does not
    have functionality to create the report
    - Some modifications to the tern executable
    
    The executable does not work
    
    Signed-off-by: Nisha K <nishak@vmware.com>

--- report/report.py
+++ report/report.py
@@ -5,12 +5,10 @@ SPDX-License-Identifier: BSD-2-Clause
 
 import logging
 
-from utils.container import check_image
-from utils.container import start_container
-from utils.container import remove_container
-from utils.container import remove_image
-from utils import constants as const
+from utils import container
+from utils import constants
 from utils import cache
+from classes.docker_image import DockerImage
 import common
 import docker
 
@@ -56,51 +54,68 @@ def load_base_image():
     return base_image
 
 
+def load_full_image():
+    '''Create image object from test image and return the object'''
+    test_image = DockerImage(docker.get_dockerfile_image_tag())
+    try:
+        test_image.load_image()
+    except NameError as error:
+        name_error_notice = Notice(test_image.repotag, str(error), 'error')
+        test_image.add_notice(name_error_notice)
+    except subprocess.CalledProcessError as error:
+        docker_exec_notice = Notice(
+            test_image.repotag, str(error.output, 'utf-8'), 'error')
+        test_image.add_notice(docker_exec_notice)
+    except IOError as error:
+        extract_error_notice = Notice(test_image.repotag, str(error), 'error')
+        base_image.add_notice(extract_error_notice)
+    return test_image
+
+
+def get_dockerfile_packages():
+    '''Given a Dockerfile return an approximate image object. This is mosty
+    guess work and shouldn't be relied on for accurate information. Add
+    Notice messages indicating as such:
+        1. Create an image with a placeholder repotag
+        2. For each RUN command, create a package list
+        3. Create layer objects with incremental integers and add the package
+        list to that layer with a Notice about parsing
+        4. Return stub image'''
+
+
 def execute_dockerfile(args):
     '''Execution path if given a dockerfile'''
     # logging
-    logger = logging.getLogger(const.logger_name)
+    logger = logging.getLogger(constants.logger_name)
     setup(args.dockerfile)
-    # ----------------------------------------------------
-    # Step 1: Get the packages installed in the base image
-    # ----------------------------------------------------
+    dockerfile_parse = False
+    # try to get Docker base image metadata
     base_image = load_base_image()
     if len(base_image.notices) == 0:
         # load any packages from cache
         if not common.load_from_cache(base_image):
             # load any packages using the command library
-            start_container(base_image.repotag)
+            container.start_container(base_image.repotag)
             common.add_base_packages(base_image)
+            container.remove_container()
+        # attempt to get the packages for the rest of the image
+        # since we only have a dockerfile, we will attempt to build the
+        # image first
+        # This step actually needs to go to the beginning but since
+        # there is no way of tracking imported images from within
+        # the docker image history, we build after importing the base image
+        build, msg = docker.is_build()
+        if build:
+            # attempt to get built image metadata
+            full_image = load_full_image()
+            if len(full_image.notices) == 0:
+                # load from Docker history
+            else:
+                # we cannot extract the built image's metadata
+                dockerfile_parse = True
+        else:
+            # we cannot build the image
+            dockerfile_parse = True
     else:
-        # don't attempt to build further; try to parse Dockerfile
-
-    # ----------------------------------------------------
-    # Step 2: Get the packages installed in the given image
-    # ----------------------------------------------------
-    # get a list of packages that may be installed from the dockerfile
-    build, msg = common.is_build()
-    if build:
-        # get the shell that we will use for all the commands
-        shell = common.get_image_shell(base_image_msg[0])
-        # start a container with the built image
-        image_tag_string = common.get_dockerfile_image_tag()
-        start_container(image_tag_string)
-        report = print_dockerfile_run(report, shell, len(base_obj_list),
-                                      master_list, args.summary, logger)
-        # remove container when done
-        remove_container()
-        remove_image(image_tag_string)
-    else:
-        # Dockerfile does not build, so get packages from dockerfile
-        # parsing only
-        pkg_dict = common.get_dockerfile_packages()
-        report = report + env_dep_dockerfile.format(build_fail_msg=msg)
-        report = report + checking_against_snippets
-        report = report + 'Packages from parsing Dockerfile RUN commands:\n'
-        for pkg in pkg_dict['recognized']:
-            report = report + ' ' + pkg
-        report = report + '\nUnregonized RUN commands in Dockerfile:\n'
-        for cmd in pkg_dict['unrecognized']:
-            report = report + cmd + '\n'
-    common.save_cache()
-    write_report(report)
+        # something went wrong in getting the base image
+        dockerfile_parse = True
