44fa3bc47292c3be3fb395e2314f7a507272cc19
Nisha K <nishak@vmware.com>
2018-05-11 09:33:18 -0700

    Improvements to rootfs utils
    The recipe to invoke commands in an isolated overlay filesystem
    is to:
    1. Mount the first layer
    2. Run the command in chroot
    3. Mount the diff layer
    4. Run the command in chroot
    
    And so on until all the layers are mounted.
    To that effect, some modifications have been made to the rootfs
    utility and the constants. Also, a sample Dockerfile that produces
    3 layer filesystems was added to test against.
    
    - Changed some constants around. The untar directory now lives in
    the same directory as the layer.tar file. There is no need for an
    exclusive upper directory for the overlay because the contents of the
    layer.tar extract can be used
    - Since we are using a lot of constants, we just import the whole
    module in rootfs.py. Modified references to constants across the file
    as a result
    - Added a root remove filesystem command as the permissions on the
    layer.tar contents are root as well. We may not need this if we
    can find a way to reliably unwind the overlay mount
    - Added a plain mount command to mount the first layer
    - Added a separate module to figure out the directory where the
    contents of layer.tar should be extracted to
    - Changed the recipe to mount the base and diff filesystems
    - Added a module to unwind the overlay mount. This needs improvement
    
    This is ongoing work for issue #37
    
    Signed-off-by: Nisha K <nishak@vmware.com>

--- utils/rootfs.py
+++ utils/rootfs.py
@@ -8,25 +8,23 @@ import subprocess
 import shutil
 import tarfile
 
-from .constants import resolv_path
-from .constants import logger_name
-from .constants import temp_folder
-from .constants import workdir
-from .constants import upperdir
-
+from . import constants
 '''
 Operations to mount container filesystems and run commands against them
 '''
 
+# remove root filesystems
+remove = ['rm', '-rf']
+
 # mount commands
+mount = ['mount', '-o', 'bind']
 mount_proc = ['mount', '-t', 'proc', '/proc']
 mount_sys = ['mount', '-o', 'bind', '/sys']
 mount_dev = ['mount', '-o', 'bind', '/dev']
-mount_run = ['mount', '-o', 'bind', '/run']
 unmount = ['umount']
 
 # enable host DNS settings
-host_dns = ['cp', resolv_path]
+host_dns = ['cp', constants.resolv_path]
 
 # unshare PID within rootfs
 unshare_pid = ['unshare', '-pf']
@@ -35,7 +33,7 @@ unshare_pid = ['unshare', '-pf']
 union_mount = ['mount', '-t', 'overlay', 'overlay', '-o']
 
 # global logger
-logger = logging.getLogger(logger_name)
+logger = logging.getLogger(constants.logger_name)
 
 
 def root_command(command, *extra):
@@ -61,13 +59,17 @@ def root_command(command, *extra):
         return result
 
 
-def extract_layer_tar(layer_tar, directory):
+def get_untar_dir(layer_tarfile):
+    '''get the directory to untar the layer tar file'''
+    return os.path.join(constants.temp_folder, os.path.dirname(
+        layer_tarfile), constants.untar_dir)
+
+
+def extract_layer_tar(layer_tar_path, directory_path):
     '''Assuming all the metadata for an image has been extracted into the
     temp folder, extract the tarfile into the required directory'''
-    tarfile_path = os.path.abspath(
-        os.path.join(temp_folder, layer_tar))
-    with tarfile.open(tarfile_path) as tar:
-        tar.extractall(directory)
+    with tarfile.open(layer_tar_path) as tar:
+        tar.extractall(directory_path)
 
 
 def prep_rootfs(rootfs_dir):
@@ -77,50 +79,68 @@ def prep_rootfs(rootfs_dir):
         root_command(mount_proc, os.path.join(rootfs_path, 'proc'))
         root_command(mount_sys, os.path.join(rootfs_path, 'sys'))
         root_command(mount_dev, os.path.join(rootfs_path, 'dev'))
-        root_command(mount_run, os.path.join(rootfs_path, 'run'))
         root_command(host_dns, os.path.join(
-            rootfs_path, resolv_path[1:]))
+            rootfs_path, constants.resolv_path[1:]))
     except subprocess.CalledProcessError as error:
         logger.error(error.output)
+        raise
 
 
-def mount_base_layer(base_rootfs):
+def mount_base_layer(base_layer_tar):
     '''To mount to base layer:
-        1. Untar the base rootfs into the working directory
-        2. Prepare all the mounts'''
-    extract_layer_tar(base_rootfs, workdir)
-    prep_rootfs(workdir)
-
-
-def mount_diff_layer(diff_rootfs):
-    '''To mount the diff layer:
-        1. Untar the diff rootfs into the upper directory
-        2. Union mount this directory on the working directory
+        1. Untar the base layer tar file
+        2. Mount into mergedir
         3. Prepare all the mounts'''
-    extract_layer_tar(diff_rootfs, upperdir)
-    args = 'lowerdir=' + workdir + ',upperdir=' + upperdir + ',workdir=' + \
-        workdir
-    root_command(union_mount, args, workdir)
-    prep_rootfs(workdir)
+    base_rootfs_path = get_untar_dir(base_layer_tar)
+    source_dir_path = os.path.join(constants.temp_folder, base_layer_tar)
+    target_dir_path = os.path.join(constants.temp_folder, constants.mergedir)
+    if os.path.isdir(base_rootfs_path):
+        root_command(remove, base_rootfs_path)
+    extract_layer_tar(source_dir_path, base_rootfs_path)
+    root_command(mount, base_rootfs_path, target_dir_path)
+    prep_rootfs(target_dir_path)
+
+
+def mount_diff_layer(diff_layer_tar):
+    '''To mount the diff layer:
+        1. Untar the diff rootfs
+        2. Union mount this directory on the mergedir
+        3. Prepare all the mounts'''
+    upper_dir_path = get_untar_dir(diff_layer_tar)
+    source_dir_path = os.path.join(constants.temp_folder, diff_layer_tar)
+    merge_dir_path = os.path.join(constants.temp_folder, constants.mergedir)
+    workdir_path = os.path.join(constants.temp_folder, constants.workdir)
+    if os.path.isdir(upper_dir_path):
+        root_command(remove, upper_dir_path)
+    extract_layer_tar(source_dir_path, upper_dir_path)
+    args = 'lowerdir=' + merge_dir_path + ',upperdir=' + upper_dir_path + \
+        ',workdir=' + workdir_path
+    root_command(union_mount, args, merge_dir_path)
+    prep_rootfs(merge_dir_path)
 
 
 def run_chroot_command(command_string, shell):
     '''Run the command string in a chroot jail within the rootfs namespace'''
+    target_dir = os.path.join(constants.temp_folder, constants.mergedir)
     mount_proc = '--mount-proc=' + os.path.join(
-        os.path.abspath(workdir), 'proc')
-    result = root_command(
-        unshare_pid, mount_proc, 'chroot', workdir, shell, '-c', command_string)
+        os.path.abspath(target_dir), 'proc')
+    result = root_command(unshare_pid, mount_proc, 'chroot', target_dir,
+                          shell, '-c', command_string)
     return result
 
 
+def unwind_mount():
+    '''Unmount all the directories'''
+    rootfs_path = os.path.join(constants.temp_folder, constants.mergedir)
+    root_command(unmount, os.path.join(rootfs_path, 'proc'))
+    root_command(unmount, os.path.join(rootfs_path, 'sys'))
+    root_command(unmount, os.path.join(rootfs_path, 'dev'))
+    root_command(unmount, '-rf', rootfs_path)
+
+
 def clean_up():
-    '''Clean up all the directories'''
-    workdir_path = os.path.abspath(workdir)
-    upperdir_path = os.path.abspath(upperdir)
-    root_command(unmount, '-r', workdir_path)
-    root_command(unmount, os.path.join(workdir_path, 'proc'))
-    root_command(unmount, os.path.join(workdir_path, 'sys'))
-    root_command(unmount, os.path.join(workdir_path, 'dev'))
-    root_command(unmount, os.path.join(workdir_path, 'run'))
+    '''Remove all the setup directories'''
+    mergedir_path = os.path.join(constants.temp_folder, constants.mergedir)
+    workdir_path = os.path.join(constants.temp_folder, constants.workdir)
+    shutil.rmtree(mergedir_path)
     shutil.rmtree(workdir_path)
-    shutil.rmtree(upperdir_path)
