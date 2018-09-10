913cbb9e56598cc96959243cfab742597d725b2f
Nisha K <nishak@vmware.com>
2018-09-10 09:16:27 -0700

    Remove inode from filesystem hash script
    inode changes when a filesystem is used to build a container image
    rather than just pull from the registry, possibly because it is
    mounted twice. Since the file stats are represented in the other
    information, the inode number can be dropped.
    
    Signed-off-by: Nisha K <nishak@vmware.com>

--- tools/fs_hash.sh
+++ tools/fs_hash.sh
@@ -5,8 +5,8 @@
 #
 # Given a file path, create a list of file stats and their sha256sums
 # usage: ./fs_hash.sh path/to/dir
-# format: inode|permissions|uid|gid|size in bytes|number of hard links|security context|sha256sum filepath
+# format: permissions|uid|gid|size in bytes|number of hard links|security context|sha256sum filepath
 
 pushd $1 > /dev/null;
-  find -type f -printf "%i|%M|%U|%G|%s|%n|%Z|" -exec sha256sum {} \;
+  find -type f -printf "%M|%U|%G|%s|%n|%Z|" -exec sha256sum {} \;
 popd > /dev/null;
