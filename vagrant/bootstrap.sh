a88ba8c3707f4c58df541b23951ae04343ed61aa
Vikram Yadav <vyadav@pivotal.io>
2018-11-07 16:55:36 -0500

    Update vagrant setup
    - Provide user with vagrant file and bootstrap script for setup to save time when running tern.
    - Resolves #79
    
    Signed-off-by: Yoon Jean Kim <ykim@pivotal.io>
    Signed-off-by: Vikram Yadav <vyadav@pivotal.io>

---
+++ vagrant/bootstrap.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+
+# Update the Ubuntu repositories
+sudo apt-get update
+
+# Upgrade all currently installed packages
+sudo apt-get -y upgrade
+
+# Python3 versions
+sudo apt-get install -y python3 python3-pip python3-venv
+
+# Install Docker
+sudo apt-get install -y docker.io
+
+# Docker adjustments
+
+# Optional: Make it easier to run Docker commands by adding
+# the vagrant user to the Docker group. If you want to run commands
+# against Docker without doing this, you will need to use sudo.
+
+sudo usermod -a -G docker vagrant
+
+# Install dependencies
+cd /tern
+pip3 install -r requirements.txt
