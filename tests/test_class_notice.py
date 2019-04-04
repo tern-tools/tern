2139bb1b306c7ed0c1482fe9edaf84258279e9ce
Nisha K <nishak@vmware.com>
2019-04-04 16:18:34 -0700

    tests: Fixed some simple class tests
    test_class_image.py:
      - Bumped copyright year
      - Used full module path
      - Added Origins class import
      - Updated test for instantiating the base image class
    test_class_image_layer.py:
      - Used full module path
    test_class_notice.py:
      - Bumped copyright year
      - Used full module path
      - Fixed test for to_dict method
    test_class_notice_origin.py:
      - Used full module path plus some rearranging of module import
        statements
    test_class_package.py:
      - Bumped copyright year
      - Used full module path
    
    Signed-off-by: Nisha K <nishak@vmware.com>

--- tests/test_class_notice.py
+++ tests/test_class_notice.py
@@ -1,12 +1,12 @@
 '''
-Copyright (c) 2017 VMware, Inc. All Rights Reserved.
+Copyright (c) 2017-2019 VMware, Inc. All Rights Reserved.
 SPDX-License-Identifier: BSD-2-Clause
 '''
 
 import unittest
 
-from classes.notice import Notice
-from classes.notice import NoticeException
+from tern.classes.notice import Notice
+from tern.classes.notice import NoticeException
 
 
 class TestClassNotice(unittest.TestCase):
@@ -33,13 +33,14 @@ class TestClassNotice(unittest.TestCase):
         self.notice.level = 'warning'
         self.assertEqual(self.notice.message, 'tag')
         self.assertEqual(self.notice.level, 'warning')
-    
+
     def testToDict(self):
         self.notice.message = 'tag'
         self.notice.level = 'warning'
         dict = self.notice.to_dict()
-        self.assertEqual(dict.message, 'tag')
-        self.assertEqual(dict.level, 'warning')
+        self.assertEqual(dict['message'], 'tag')
+        self.assertEqual(dict['level'], 'warning')
+
 
 if __name__ == '__main__':
     unittest.main()
