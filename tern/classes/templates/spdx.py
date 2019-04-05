47c80d5960cbff3dd29746c5a5d10b44890878b8
Nisha K <nishak@vmware.com>
2019-04-05 13:58:54 -0700

    classes: Added initial template - SPDX
    This is the first attempt at creating a custom template.
    The SPDX template is work towards #174 and resolves #185
    
    The key mappings only use properties that overlap between the current
    class properties and SPDX document tags. Other mandatory SPDX tags
    will be added later.
    
    Signed-off-by: Nisha K <nishak@vmware.com>

---
+++ tern/classes/templates/spdx.py
@@ -0,0 +1,25 @@
+#
+# Copyright (c) 2019 VMware, Inc. All Rights Reserved.
+# SPDX-License-Identifier: BSD-2-Clause
+#
+
+from tern.classes.template import Template
+
+
+class SPDXTagValue(Template):
+    '''This is the SPDX Template class
+    It provides mappings for the SPDX tag-value document format'''
+
+    def package(self):
+        return {'name': 'PackageName',
+                'version': 'PackageVersion',
+                'license': 'PackageLicenseDeclared'}
+
+    def image_layer(self):
+        return {'diff_id': 'PackageName',
+                'fs_hash': 'PackageChecksum'}
+
+    def image(self):
+        return {'name': 'PackageName',
+                'tag': 'PackageVersion',
+                'id': 'PackageChecksum'}
