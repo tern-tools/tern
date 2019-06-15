4953670c9db55b966a66a0a254e86a2900323b5b
Rose Judge <rjudge@vmware.com>
2019-06-14 17:35:19 -0700

    ci: Don't run prospector for .md file changes
    Currently Prospector runs for all pull requests but in reality, we only
    need to run Prospector on changes in non-*.md files.
    
    This commit introduces the following changes:
    
      1) Adds evaluate_docs.py to the 'ci' directory. This script will fail
         if the only changes made were to *.md files.
      2) Modifies .circleci/config.yml to only run Prospector if the
         evaluate_docs.py test passes.
    
    Signed-off-by: Rose Judge <rjudge@vmware.com>

---
+++ ci/evaluate_docs.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2019 VMware, Inc. All Rights Reserved.
+# SPDX-License-Identifier: BSD-2-Clause
+
+from git import Repo
+import os
+import sys
+
+
+# This script is designed to be run with circleci.
+# This script should pass if we need to run Bandit and Prospector,
+# which means that there are files other than *.md files being changed.
+# If only *.md files are changed, we don't need to run Bandit
+# or Prospector and the script will fail.
+
+# Assume that all files are *.md until proven otherwise 
+docs_only = True
+
+
+repo = Repo(os.getcwd())
+repo.git.remote('add', 'upstream', 'git@github.com:vmware/tern.git')
+repo.git.fetch('upstream')
+
+hcommit = repo.head.commit
+diff = hcommit.diff('upstream/master')
+
+changes=[]
+for d in diff:
+    # Get the list of strings for changed files
+    changes.append(d.b_path)
+
+# check that changes has entries
+if not changes:
+    print('No changes to run tests for.')
+    sys.ext(1)
+
+for change in changes:
+    if change[-3:] != '.md': 
+        docs_only = False
+        break
+
+if docs_only:
+    sys.exit(1)
+else:
+    sys.exit(0)
