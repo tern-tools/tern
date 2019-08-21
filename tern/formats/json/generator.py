1a69aef284fb9f70b358c9778150bcb24906f331
Rose Judge <rjudge@vmware.com>
2019-08-21 15:22:44 -0700

    Separate json formatting into its own plugin
    This commit separates the json reporting code currently under Tern's
    core code and puts it in its own plugin that Stevedore can utilize.
    Specifically, the following changes were made:
    
    1) Add json entrypoint to setup.cfg.
    
    2) Remove the -j and --json command line option in tern/__main__.py.
       Moving forward we will utilize Stevedore to manage the plugin
       selection at runtime. Similar to the spdxtagvalue plugin, users can
       invoke the json reporting format by using "-m json" on the
       command line.
    
    3) Copy the body of print_json_report() in tern/report/content.py
       and move it under generate() in tern/formats/json/generator.py.
    
    4) Remove code corresponding to the -j command line option in
       tern/report/report.py.
    
    5) Remove json_file constant in tern/utils/constants.py as it is no
       longer used.
    
    6) Update the circleci test to run the json reporting using
       "-m json" instead of the deprecated -j command line option.
    
    Resolves #370
    
    Signed-off-by: Rose Judge <rjudge@vmware.com>

---
+++ tern/formats/json/generator.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2019 VMware, Inc. All Rights Reserved.
+# SPDX-License-Identifier: BSD-2-Clause
+
+"""
+JSON document generator
+"""
+
+import json
+from tern.formats import generator
+
+
+class JSON(generator.Generate):
+    def generate(self, image_obj_list):
+        '''Given a list of image objects, create a json object string'''
+        image_list = []
+        for image in image_obj_list:
+            image_list.append({'image': image.to_dict()})
+        image_dict = {'images': image_list}
+        return json.dumps(image_dict)
