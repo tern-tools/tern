d52bfd17b34de6805b37399893fdd2ce0c255493
Rose Judge <rjudge@vmware.com>
2019-12-02 21:28:00 -0800

    Clean working dir the same for --bind-mount option
    tern/report/report.py currently cleans up the working directory
    differently if the --bind-mount CLI option is enabled. Previous commits
    have changed the way the working directory is set, however, and this
    commit makes changes to the clean_working_dir() function to reflect
    that. Instead of checking to see if the bind_mount option is enabled,
    clean the working directory provided by rootfs.get_working_dir()
    
    Works towards #498
    
    Signed-off-by: Rose Judge <rjudge@vmware.com>

--- tern/report/report.py
+++ tern/report/report.py
@@ -82,17 +82,7 @@ def clean_working_dir(bind_mount):
     If bind_mount is true then leave the upper level directory'''
     path = rootfs.get_working_dir()
     if os.path.exists(path):
-        if bind_mount:
-            # clean whatever is in temp_folder without removing the folder
-            inodes = os.listdir(path)
-            for inode in inodes:
-                dir_path = os.path.join(path, inode)
-                if os.path.isdir(dir_path):
-                    shutil.rmtree(dir_path)
-                else:
-                    os.remove(dir_path)
-        else:
-            shutil.rmtree(path)
+        shutil.rmtree(path)
 
 
 def load_base_image():
