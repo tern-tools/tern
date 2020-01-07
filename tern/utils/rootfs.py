4583c5f45e7688e04840dfc347775f5261c9566c
Nisha K <nishak@vmware.com>
2020-01-07 09:18:07 -0800

    utils: Update shell_command calls
    This is work towards allowing extensions to run as root.
    
    We've updated the shell_command function to take in a boolean
    positional argument. Modify this function call in the rest of the
    code such that it takes this argument. This change allows the
    tar check and extraction to be run without elevated privileges as
    some tarballs are not created by a root user.
    
    Signed-off-by: Nisha K <nishak@vmware.com>

--- tern/utils/rootfs.py
+++ tern/utils/rootfs.py
@@ -99,7 +99,8 @@ def shell_command(is_sudo, command, *extra):
 def check_tar_permissions(tar_file, directory_path):
     '''Invoke a shell command as the current user. If the error contains
     'Operation not permitted' then return False. Else return True'''
-    _, error = shell_command(extract_tar, tar_file, '-C', directory_path)
+    _, error = shell_command(
+        False, extract_tar, tar_file, '-C', directory_path)
     if "Operation not permitted" in error.decode():
         return False
     return True
@@ -108,7 +109,7 @@ def check_tar_permissions(tar_file, directory_path):
 def check_tar_members(tar_file):
     '''Given the path to the tar file, check to see if there is an error with
     the members of the tarfile or if it is empty'''
-    result, error = shell_command(check_tar, tar_file)
+    result, error = shell_command(False, check_tar, tar_file)
     if error:
         logger.error("Malformed tar: %s", error.decode())
         raise EOFError("Malformed tarball: {}".format(tar_file))
