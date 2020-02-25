6edc4c8178185a63238e052a702b5f5da67ce82c
Nisha K <nishak@vmware.com>
2020-02-25 12:33:20 -0800

    container: close the client socket
    The docker client socket isn't closed after the analysis is done.
    We define a close_client function that will try to close any
    open socket and ignore any errors where there is no connection as
    that is an indication that no socket is open.
    
    Closed the client socket in the teardown for the unit test for
    the DockerImage class. Did a small change in the line length so it
    is less than 80 characters long to maintain code style.
    
    Signed-off-by: Nisha K <nishak@vmware.com>

--- tern/analyze/docker/container.py
+++ tern/analyze/docker/container.py
@@ -165,3 +165,14 @@ def extract_image_metadata(image_tag_string):
             raise IOError('Unable to untar Docker image')
     except docker.errors.APIError:  # pylint: disable=try-except-raise
         raise
+
+
+def close_client():
+    '''End docker interactions by closing the client. This is meant to be
+    used after analysis is done'''
+    try:
+        client.close()
+    except requests.exceptions.ConnectionError:
+        # it should either already be closed or docker is not setup
+        # either way, the socket is closed
+        pass
