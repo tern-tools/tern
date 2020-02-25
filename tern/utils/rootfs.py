b05f7057ff59cbc79f0202994dc93a2507421119
abhay <abhay.katheria1998@gmail.com>
2020-02-25 11:08:18 +0530

    Added conditional to check the mountpoints
    Earlier there were no checks to check the existence of the
    proc,sys and dev mount points . Due to that OpenSUSE tumbleweed
    image scan was failing.
    
    I created addtional checks and made the mount points
    if they don't exist.
    
    Resolves #538
    
    Signed-off-by: abhay <abhay.katheria1998@gmail.com>

--- tern/utils/rootfs.py
+++ tern/utils/rootfs.py
@@ -176,6 +176,15 @@ def extract_tarfile(tar_path, directory_path):
 def prep_rootfs(rootfs_dir):
     '''Mount required filesystems in the rootfs directory'''
     rootfs_path = os.path.abspath(rootfs_dir)
+    proc_path = os.path.join(rootfs_path, 'proc')
+    sys_path = os.path.join(rootfs_path, 'sys')
+    dev_path = os.path.join(rootfs_path, 'dev')
+    if not os.path.exists(proc_path):
+        os.mkdir(proc_path)
+    if not os.path.exists(sys_path):
+        os.mkdir(sys_path)
+    if not os.path.exists(dev_path):
+        os.mkdir(dev_path)
     try:
         root_command(mount_proc, os.path.join(rootfs_path, 'proc'))
         root_command(mount_sys, os.path.join(rootfs_path, 'sys'))
