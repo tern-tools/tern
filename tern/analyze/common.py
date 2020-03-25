6453df025c8f650f32719316cf667e92218eeb49
Nisha K <nishak@vmware.com>
2020-03-25 13:27:36 -0700

    analyze: Find the base OS binary without mounting
    This commit modifies the get_base_bin function to take an ImageLayer
    object and return an associated binary known by the command library.
    This allows the first layer to not be mounted in order to search
    for the binary by walking the directory structure. This also allows
    mounting and unmounting to occur only when trying to execute
    commands in a chroot environment on a mounted filesystem.
    
    Signed-off-by: Nisha K <nishak@vmware.com>

--- tern/analyze/common.py
+++ tern/analyze/common.py
@@ -125,13 +125,13 @@ def save_to_cache(image):
             cache.add_layer(layer)
 
 
-def get_base_bin():
-    '''Given the base layer object, find the binary used to identify the
-    base OS layer. Assume that the layer filesystem is mounted'''
+def get_base_bin(first_layer):
+    '''Find the binary used to identify the base OS for the container image.
+    We do this by providing the path to the first layer of the container
+    image and looking for known binaries there. Assume that the layer has
+    already been unpacked with the filesystem'''
     binary = ''
-    # the path to where the filesystem is mounted
-    # look at utils/rootfs.py mount_base_layer module
-    cwd = os.path.join(rootfs.get_working_dir(), constants.mergedir)
+    cwd = rootfs.get_untar_dir(first_layer.tar_file)
     for key, value in command_lib.command_lib['base'].items():
         for path in value['path']:
             if os.path.exists(os.path.join(cwd, path)):
