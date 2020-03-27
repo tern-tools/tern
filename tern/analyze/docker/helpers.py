2c6ca3d329ea1befa487fdcee905d20420b8c371
Rose Judge <rjudge@vmware.com>
2020-03-26 17:58:19 -0700

    Update parsing functions to use dockerfile objects
    This commit makes changes to tern/analyze/docker/run.py and
    tern/analyze/docker/helpers.py to utilize dockerfile objects for
    parsing purposes when applicable. The dockerfile object information
    is accessible via the global 'docker_commands' variable assigned as
    part of the setup process in helpers.py.
    
    This commit also makes changes to the appropriate functions to
    utilize the new dfile_lock and dfobj arguments. In order to pass
    the dfobj as a flag to analyze-related functions, a dockerfile
    object is created in execute_dockerfile() based on the dockerfile
    path that was provided on the command line when Tern was run.
    
    The most significant changes in helpers.py is to the
    get_dockerfile_base() function. The function still serves the same
    purpose it did before this commit, but now gets information about
    the base tag directly from the dockerfile object and adds more error
    checking around various dockerfile keywords related to the FROM
    command.
    
    Works towards #522
    
    Signed-off-by: Rose Judge <rjudge@vmware.com>

--- tern/analyze/docker/helpers.py
+++ tern/analyze/docker/helpers.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (c) 2017-2019 VMware, Inc. All Rights Reserved.
+# Copyright (c) 2017-2020 VMware, Inc. All Rights Reserved.
 # SPDX-License-Identifier: BSD-2-Clause
 
 """
@@ -11,6 +11,7 @@ import docker
 import logging
 import os
 import re
+import sys
 
 from tern.classes.docker_image import DockerImage
 from tern.classes.notice import Notice
@@ -20,6 +21,7 @@ from tern.utils import constants
 from tern.report import errors
 from tern.report import formats
 from tern.analyze import common
+from tern.utils.general import check_image_string
 
 # dockerfile
 dockerfile_global = ''
@@ -30,25 +32,14 @@ docker_commands = []
 logger = logging.getLogger(constants.logger_name)
 
 
-def load_docker_commands(dockerfile_path):
-    '''Given a dockerfile get a persistent list of docker commands'''
-    if not os.path.isfile(dockerfile_path):
-        raise IOError('{} does not exist'.format(dockerfile_path))
+def load_docker_commands(dfobj):
+    '''Given a dockerfile object get a persistent list of docker commands'''
+    if not os.path.isfile(dfobj.filepath):
+        raise IOError('{} does not exist'.format(dfobj.filepath))
     global docker_commands
-    docker_commands = dockerfile.get_directive_list(
-        dockerfile.get_command_list(dockerfile_path))
+    docker_commands = dfobj.structure
     global dockerfile_global
-    dockerfile_global = dockerfile_path
-
-
-def print_dockerfile_base(base_instructions):
-    '''For the purpose of tracking the lines in the dockerfile that
-    produce packages, return a string containing the lines in the dockerfile
-    that pertain to the base image'''
-    base_instr = ''
-    for instr in base_instructions:
-        base_instr = base_instr + instr[0] + ' ' + instr[1]
-    return base_instr
+    dockerfile_global = dfobj.filepath
 
 
 def get_dockerfile_base():
@@ -56,42 +47,60 @@ def get_dockerfile_base():
     1. get the instructions around FROM
     2. get the base image and tag
     3. Make notes based on what the image and tag rules are
-    4. Return an image object and the base instructions string'''
+    4. Return an image object and the base instructions string
+    NOTE: Potential ARG values in the Dockerfile object have already been
+    expanded at this point. However, Dockerfile rules say that if no
+    --build-arg is passed during docker build and ARG has no default, the
+    build will fail. We assume for now that we will not be passing build
+    arguments in which case if there is no default ARG, we will raise an
+    exception indicating that since the build arguments are determined by
+    the user we will not be able to determine what the user wanted'''
     try:
-        base_instructions = dockerfile.get_base_instructions(docker_commands)
-        base_image_tag = dockerfile.get_base_image_tag(base_instructions)
-        dockerfile_lines = print_dockerfile_base(base_instructions)
+        dockerfile_lines = docker_commands
+        base_image_string = ''
+        from_line = ''
+        # Get the base image tag.
+        # NOTE: ARG values have already been expanded.
+        for i, cmd_dict in enumerate(dockerfile_lines):
+            if cmd_dict['instruction'] == 'FROM':
+                # Account for "as" keyword in FROM line
+                base_image_string = re.split(" as", cmd_dict['value'],
+                                             flags=re.IGNORECASE)[0]
+                from_line = 'FROM' + base_image_string
+                # Check that potential ARG values has default
+                if i != 0 and dockerfile_lines[i-1]['instruction'] == 'ARG':
+                    if len(dockerfile_lines[i-1]['value'].split('=')) == 1:
+                        raise ValueError('No ARG default value to pass to '
+                                         'FROM command in Dockerfile.')
+                break
         # check for scratch
-        if base_image_tag[0] == 'scratch':
-            # there is no base image - return no image object
-            return None, None
+        if base_image_string == 'scratch':
+            # there is no base image to pull
+            raise ValueError("Cannot pull 'scratch' base image.")
         # there should be some image object here
-        repotag = base_image_tag[0] + dockerfile.tag_separator + \
-            base_image_tag[1]
-        from_line = 'FROM ' + repotag
-        base_image = DockerImage(repotag)
-        base_image.origins.add_notice_origin(dockerfile_lines)
-        base_image.name = base_image_tag[0]
+        base_image = DockerImage(base_image_string)
+        base_image.origins.add_notice_origin(from_line)
+        base_image.name = base_image_string.split(':')[0]
         # check if there is a tag
-        if not base_image_tag[1]:
+        if not check_image_string(base_image_string):
             message_string = errors.dockerfile_no_tag.format(
                 dockerfile_line=from_line)
             base_image.origins.add_notice_to_origins(
                 dockerfile_lines, Notice(message_string, 'warning'))
             base_image.tag = 'latest'
         else:
-            base_image.tag = base_image_tag[1]
+            base_image.tag = base_image_string.split(':')[1]
         # check if the tag is 'latest'
-        if base_image_tag[1] == 'latest':
+        if base_image.tag == 'latest':
             message_string = errors.dockerfile_using_latest.format(
                 dockerfile_line=from_line)
             base_image.origins.add_notice_to_origins(
                 dockerfile_lines, Notice(message_string, 'warning'))
-        return base_image, dockerfile_lines
+        return base_image, from_line
     except ValueError as e:
-        logger.warning("%s", errors.cannot_parse_base_image.format(
+        logger.fatal("%s", errors.cannot_parse_base_image.format(
             dockerfile=dockerfile_global, error_msg=e))
-        return None, None
+        sys.exit(1)
 
 
 def get_dockerfile_image_tag():
@@ -167,16 +176,17 @@ def set_imported_layers(docker_image):
     '''Given a Docker image object that was built from a Dockerfile, set the
     layers that were imported using the Dockerfile's FROM command or the ones
     that came before it'''
-    dockerfile_lines = dockerfile.get_command_list(dockerfile_global)
     index = -1
     from_line = ''
-    for line in dockerfile_lines:
-        if 'FROM' in line:
-            from_line = line
+    dockerfile_lines = docker_commands
+    for cmd in dockerfile_lines:
+        if cmd['instruction'] == 'FROM':
+            from_line = cmd['content'].rstrip()
             break
+    command_list = dockerfile.get_command_list(dockerfile_lines)
     for layer in docker_image.layers:
         instr = created_to_instruction(layer.created_by)
-        if instr in dockerfile_lines:
+        if instr in command_list:
             index = docker_image.layers.index(layer)
             break
     if index != -1:
