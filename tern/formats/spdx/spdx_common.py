e51d3611786e2251e1329bf6484bebaabc601fdd
Rose Judge <rjudge@vmware.com>
2021-01-11 16:50:00 -0800

    Create spdx_common.py for common functions
    Adding a new SPDX JSON format means that we can utilize some of the
    existing tag-value functions when creating JSON documents. Create
    spdx/spdx_common.py as a place to put common functions.
    
    Works towards #839
    
    Signed-off-by: Rose Judge <rjudge@vmware.com>

---
+++ tern/formats/spdx/spdx_common.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2021 VMware, Inc. All Rights Reserved.
+# SPDX-License-Identifier: BSD-2-Clause
+
+"""
+Common functions that are useful for both JSON and Tag-value document creation
+"""
