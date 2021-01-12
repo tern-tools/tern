bca41a2eedb52dcb1862736c7730713e696a48d7
Rose Judge <rjudge@vmware.com>
2021-01-11 17:57:44 -0800

    Add support for SPDX JSON format
    This commit adds a new directory, spdx/spdxjson that contains a handful
    of files and functions which support the creation of a new SPDX JSON
    format.
    
    1) generator.py
        The primary SPDX JSON document generator. Implements the abstract
        'generate' function and creates a new class, SpdxJSON. Makes calls
        to the various helper functions within the spdx/spdxjson directory.
    
    2) image_helpers.py, layer_helpers.py, package_helpers.py, and
       file_helpers.py
        Helper files that contain helper functions related to gathering
        and formatting data for the image, layers, packages and files,
        respectively.
    
    3) formats.py
        Contains SPDX JSON specific document formatting and strings.
    
    Resolves #839
    
    Signed-off-by: Rose Judge <rjudge@vmware.com>

---
+++ tern/formats/spdx/spdxjson/generator.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2021 VMware, Inc. All Rights Reserved.
+# SPDX-License-Identifier: BSD-2-Clause
+
+"""
+SPDX JSON document generator
+"""
+
+import json
+import datetime
+import logging
+
+from tern.formats.spdx.spdx import SPDX
+from tern.formats.spdx import spdx_common
+from tern.utils.general import get_git_rev_or_version
+from tern.utils import constants
+from tern.formats.spdx.spdxjson import formats as json_formats
+from tern.formats.spdx.spdxjson import image_helpers as mhelpers
+from tern.formats.spdx.spdxjson import file_helpers as fhelpers
+from tern.formats.spdx.spdxjson import layer_helpers as lhelpers
+from tern.formats.spdx.spdxjson import package_helpers as phelpers
+from tern.formats import generator
+
+
+# global logger
+logger = logging.getLogger(constants.logger_name)
+
+
+def get_document_namespace(image_obj):
+    '''Given the image object, return a unique SPDX document uri.
+    This is a combination of the tool name and version, the image name
+    and the uuid'''
+    return json_formats.document_namespace.format(
+        version=get_git_rev_or_version()[1], image=image_obj.name,
+        uuid=spdx_common.get_uuid())
+
+
+def get_document_dict(image_obj, template):
+    '''Return document info as a dictionary'''
+    # docu_dict = {"Document": {}}
+    docu_dict = {
+        'SPDXID': json_formats.spdx_id,
+        'spdxVersion': json_formats.spdx_version,
+        'creationInfo': {
+            'created': json_formats.created.format(
+                timestamp=datetime.datetime.utcnow().strftime(
+                    "%Y-%m-%dT%H:%M:%SZ")),
+            'creators': json_formats.creator.format(
+                version=get_git_rev_or_version()[1]),
+            'licenseListVersion': json_formats.license_list_version,
+        },
+        'name': json_formats.document_name.format(image_name=image_obj.name),
+        'dataLicense': json_formats.data_license,
+        'comment': json_formats.document_comment,
+        'documentNamespace': get_document_namespace(image_obj),
+        'documentDescribes': [spdx_common.get_image_spdxref(image_obj)],
+        'packages': [
+            # image dict will be a single dictionary
+            # we'll add the layer and package dicts later if available
+            mhelpers.get_image_dict(image_obj, template)],
+        'relationships': lhelpers.get_image_layer_relationships(image_obj)
+    }
+
+    # Add list of layer dictionaries to packages list
+    docu_dict['packages'] += lhelpers.get_layers_list(image_obj, template)
+
+    # Add list of package dictionaries to packages list, if they exist
+    pkgs_dict_list = phelpers.get_packages_list(image_obj, template)
+    if pkgs_dict_list:
+        docu_dict['packages'] += pkgs_dict_list
+
+    # Add list of file dictionaries, if they exist
+    files = fhelpers.get_files_list(image_obj, template)
+    if files:
+        docu_dict['files'] = files
+
+    return docu_dict
+
+
+class SpdxJSON(generator.Generate):
+    def generate(self, image_obj_list):
+        '''Generate an SPDX document
+        WARNING: This assumes that the list consists of one image or the base
+        image and a stub image, in which case, the information in the stub
+        image is not applicable in the SPDX case as it is an empty image
+        object with no metadata as nothing got built.
+        The whole document should be stored in a dictionary which can be
+        converted to JSON and dumped to a file using the write_report function
+        in report.py.
+
+        For the sake of SPDX, an image is a 'Package' which 'CONTAINS' each
+        layer which is also a 'Package' which 'CONTAINS' the real Packages'''
+        logger.debug("Generating SPDX JSON document...")
+
+        # we still don't know how SPDX documents could represent multiple
+        # images. Hence we will assume only one image is analyzed and the
+        # input is a list of length 1
+        image_obj = image_obj_list[0]
+        template = SPDX()
+        report = get_document_dict(image_obj, template)
+
+        return json.dumps(report)
---
+++ tern/formats/spdx/spdxjson/layer_helpers.py
@@ -0,0 +1,166 @@
+# -*- coding: utf-7 -*-
+#
+# Copyright (c) 2021 VMware, Inc. All Rights Reserved.
+# SPDX-License-Identifier: BSD-2-Clause
+
+"""
+Helpers for layer information
+Layers for SPDX act like a Package
+"""
+import logging
+import os
+
+from tern.utils import constants
+from tern.formats.spdx import spdx_common
+from tern.formats.spdx.spdxjson import formats as json_formats
+from tern.report import content
+
+
+# global logger
+logger = logging.getLogger(constants.logger_name)
+
+
+def get_image_layer_relationships(image_obj):
+    '''Given an image object, return a list of dictionaries describing the
+    relationship between each layer "package" and the image and packages
+    related to it.
+    For SPDX JSON format this will typically look like:
+    {
+      "spdxElementId" : "SPDXRef-image",
+      "relatedSpdxElement" : "SPDXRef-layer",
+      "relationshipType" : "CONTAINS"
+    }'''
+    layer_relationships = []
+    image_ref = spdx_common.get_image_spdxref(image_obj)
+
+    # Required - DOCUMENT_DESCRIBES relationship
+    layer_relationships.append(json_formats.get_relationship_dict(
+        json_formats.spdx_id, image_ref, 'DESCRIBES'))
+
+    for index, layer in enumerate(image_obj.layers):
+        layer_ref = spdx_common.get_layer_spdxref(layer)
+        # Create a list of dictionaries.
+        # First, add dictionaries for the layer relationship to the image
+        layer_relationships.append(json_formats.get_relationship_dict(
+            image_ref, layer_ref, 'CONTAINS'))
+        # Next, add dictionary of the layer relationship to other layers
+        if index != 0:
+            prev_layer_ref = spdx_common.get_layer_spdxref(
+                image_obj.layers[index - 1])
+            layer_relationships.append(json_formats.get_relationship_dict(
+                prev_layer_ref, layer_ref, 'HAS_PREREQUISITE'))
+        # Finally, add package releationships for the layer
+        if layer.packages:
+            for package in layer.packages:
+                pkg_ref = spdx_common.get_package_spdxref(package)
+                layer_relationships.append(json_formats.get_relationship_dict(
+                    layer_ref, pkg_ref, 'CONTAINS'))
+
+    return layer_relationships
+
+
+def get_layer_package_comment(layer_obj):
+    '''Return a package comment string value for a list of NoticeOrigin
+    objects for the given layer object'''
+    comment = ''
+    if "headers" in layer_obj.extension_info.keys():
+        for header in layer_obj.extension_info.get("headers"):
+            comment += header
+            comment += '\n'
+    if not layer_obj.origins.is_empty():
+        for notice_origin in layer_obj.origins.origins:
+            comment += content.print_notices(notice_origin, '', '\t')
+    return comment
+
+
+def get_layer_file_data_list(layer_obj):
+    '''Given a layer object and the template object, return the SPDX list of
+    of file refs in the layer. Return an empty string if the files are not
+    analyzed'''
+    layer_lics = []
+    if layer_obj.files_analyzed:
+        layer_checksum = spdx_common.get_layer_checksum(layer_obj)
+        # some files are located in different places in the filesystem
+        # they would occur as duplicates in this list
+        # keep a list of previously printed file spdx-refs
+        file_refs = set()
+        # file data
+        for filedata in layer_obj.files:
+            # we use the layer checksum as the layer id
+            file_ref = spdx_common.get_file_spdxref(filedata, layer_checksum)
+            if file_ref not in file_refs:
+                layer_lics.append(spdx_common.get_file_spdxref(
+                    filedata, layer_checksum))
+                file_refs.add(file_ref)
+    return layer_lics
+
+
+def get_layer_dict(layer_obj, template, image_loc=''):
+    '''Given an layer object and its SPDX template mapping, return a SPDX
+    JSON/dictionary representation of the layer. An image layer in
+    SPDX behaves like a Package. The analyzed files will go in a separate
+    dictionary for the JSON document.
+
+    We also pass the image location as optional for where the layers were
+    downloaded from. Registries can be implemented as distributed storage
+    or images can be stored as whole tarballs. Either way, the layer
+    would be downloaded along with the image.'''
+    mapping = layer_obj.to_dict(template)
+    layer_dict = {
+        'name': os.path.basename(layer_obj.tar_file),
+        'SPDXID': spdx_common.get_layer_spdxref(layer_obj),
+        'fileName': layer_obj.tar_file,
+        'downloadLocation': mapping['PackageFileName'] if image_loc
+        else 'NOASSERTION',
+        'filesAnalyzed': 'true' if layer_obj.files_analyzed else 'false',
+        'checksums': [{
+            'algorithm':
+                spdx_common.get_layer_checksum(layer_obj).split(': ')[0],
+            'checksumValue':
+                spdx_common.get_layer_checksum(layer_obj).split(': ')[1]
+        }],
+        'licenseConcluded': 'NOASSERTION',  # always NOASSERTION
+        'licenseDeclared': 'NOASSERTION',  # always NOASSERTION
+        'copyrightText': 'NOASSERTION',  # always NOASSERTION
+    }
+
+    # Only include layer file information if file data is available
+    if layer_obj.files_analyzed:
+        layer_dict['hasFiles'] = get_layer_file_data_list(layer_obj)
+
+    # packageVerificationCode must be omitted if filesAnalyzed is false
+    if layer_obj.files_analyzed:
+        layer_dict['packageVerificationCode'] = {
+            'packageVerificationCodeValue':
+            spdx_common.get_layer_verification_code(layer_obj)
+        }
+
+    # Include layer package comment only if it exists
+    layer_pkg_comment = get_layer_package_comment(layer_obj)
+    if layer_pkg_comment:
+        layer_dict['comment'] = layer_pkg_comment
+
+    # Include layer licenses from files only if they exist
+    layer_licenses = spdx_common.get_layer_licenses(layer_obj)
+    if layer_licenses:
+        layer_dict['licenseInfoFromFiles'] = layer_licenses
+
+    return layer_dict
+
+
+def get_layers_list(image_obj, template):
+    '''Given an image object and the template object for SPDX, return a list
+    of SPDX dictionary representations of each of the layers in the image.
+    Each layer will be represented as a package and hence follows the JSON
+    spec for packages.
+        name
+        versionInfo
+        downloadLocation'''
+    layer_dicts = []
+    mapping = image_obj.to_dict(template)
+    for layer in image_obj.layers:
+        # Create a list of dictionaries. Each dictionary represents one
+        # layer as a SPDX package
+        layer_dicts.append(get_layer_dict(layer, template,
+                                          mapping['PackageDownloadLocation']))
+    return layer_dicts
