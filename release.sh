821b0ae25b72130436cf73a9b11cec2f73925382
Dhairya Jain <jaindhairya2001@gmail.com>
2021-03-13 13:31:58 +0530

    Automate Tern's Release Process
    Tern's release process is very manual and cumbersome as of now. This is
    an attempt to automate it to a certain extent using shell scripts.
    Having at least a portion of this process automated will save time and
    reduce errors.
    
    Resolves: #885
    
    Signed-off-by: Dhairya Jain jaindhairya2001@gmail.com

--- release.sh
+++ release.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
 # Author: Dhairya Jain
 # Task: Automation Script for automating the release of tern
-# Description:
-#    Automate Tern's Release Process
-#
-#    Tern's release process is very manual and cumbersome as of now. This is
-#    an attempt to automate it to a certain extent using shell scripts.
-#    Having at least a portion of this process automated will save time and
-#    reduce errors.
-#
-#    Resolves: #885
-#
-#    Signed-off-by: Dhairya Jain jaindhairya2001@gmail.com
-#
-
+# Description: Automates Tern's Release Process
 
 ########### PART 1 : Prepare Release PR ################
 # After Freezing the development on main branch
@@ -24,32 +12,55 @@ DIR=$(pwd) #This is the deployment directory
 BASE_DIR=$(dirname "$(pwd)") #This is the base directory
 echo "Current Directory is: $DIR"
 echo "Base Directory used will be $BASE_DIR"
-echo "Do you agree? [y/n]"
-read -r proceed
-case $proceed in
-  n)
-    exit
-  ;;
-*)
-  ;;
-esac
+echo "Do you agree? [y/n]:- "
+l=1
+while [ $l -eq 1 ]; do
+  read -r proceed
+  case $proceed in
+    y|Y)
+      echo "Continuing . . ."
+      l=0
+      ;;
+    n|N)
+      exit
+      ;;
+    *)
+      echo "Invalid Input."
+      echo "Do you agree? [y/n]:- "
+      ;;
+  esac
+done
 
 # Prepare your local development environment by committing or stashing your changes. Work at the tip of main.
+echo "These are the pending changes in this branch"
+echo
+git diff
+
 echo "You should either commit[c]/stash[s] the changes: "
-read -r commit_or_stash
-case $commit_or_stash in
-  c)
-    git commit && git push origin main
-    echo "Committed the changes on Development Branch"
-    ;;
-s)writing the release notes etc
-  git stash
-  echo "Stashed the changes"
-  ;;
-*)
-  echo "Unknown command. Exiting ..."
-  exit
-esac
+l=1
+while [ $l -eq 1 ]; do
+  read -r commit_or_stash
+  case $commit_or_stash in
+    c)
+      git add && git commit && git push origin main
+      echo "Committed the changes on Development Branch"
+      l=0
+      ;;
+    s)
+      git stash
+      echo "Stashed the changes"
+      l=0
+      ;;
+    e)
+      exit
+      ;;
+    *)
+      echo "Unknown command. Try Again ..."
+      echo "You should either commit[c]/stash[s] the changes or input e to exit: "
+      ;;
+  esac
+done
+
 #Create a branch for the release: git checkout -b <release branch name>.
 echo "Enter the name of release branch: "
 read -r rBranch
@@ -74,14 +85,22 @@ cd "$DIR" || (echo "cd Failed" && exit )
 # Some Manual work
 echo "Compare the module versions in upgrade.txt with requirements.txt in the development environment. Bump up versions if needed."
 echo "Press Y when you are done updating and want to continue? [y/n]: "
-read -r manualWork1
-case $manualWork1 in
-  n)
-    exit
-  ;;
-*)
-  ;;
-esac
+l=1
+while [ $l -eq 1 ]; do
+  read -r manualWork1
+  case $manualWork1 in
+    n|N)
+      exit
+      ;;
+    y|Y)
+      echo "Continuing . . ."
+      l=0
+      ;;
+    *)
+      echo "Unknown command. Try Again:- "
+      ;;
+  esac
+done
 
 #Part 2b :Run Tests
 cd "$BASE_DIR/.release/ternenv/tern" || (echo "cd Failed" && exit )
@@ -94,36 +113,47 @@ cp "v$release_version-requirements.txt" "$DIR/docs/releases/"
 
 #################### Part 3: Write release notes. ################################
 cd "$DIR/docs/releases/" || (echo "cd Failed" && exit )
-echo "Write release notes all this stuff manually (type n) or Enter the path to the release notes file:- "
-read -r release_notes_path
-case $manualWork2 in
-  n)
-    touch "v$release_version.md"
-    echo "Press Y when you are done writing the release notes etc and want to continue? [y/n]: "
-    read -r manualWork2
-    case $manualWork2 in
-      n)
-        exit
+echo "Choose among the 2 options:"
+echo "1. Write release notes all this stuff manually"
+echo "2. Enter the path to the release notes file"
+echo "Your choice [1/2]:- "
+l=1
+while [ $l -eq 1 ]; do
+  read -r release_notes_path
+  case $manualWork2 in
+    1)
+      touch "v$release_version.md"
+      echo "Press any key (or y) when you are done writing the release notes and want to continue; press n otherwise."
+      echo "Your response [y/n]: "
+      read -r manualWork2
+      case $manualWork2 in
+        n|N)
+          exit
+          ;;
+        *)
+          ;;
+      esac
+      ;;
+    2)
+      mv "$release_notes_path" ./"v$release_version.md"
       ;;
     *)
+      echo "Invalid option. Try Again:- "
       ;;
-    esac
-  ;;
-  *)
-    mv "$release_notes_path" ./"v$release_version.md"
-  ;;
-esac
+  esac
+done
 #################### Part 4: Commit release notes and submit a PR and Tag release on Git################################
 git add && git commit
 echo "Do the github works: Commit Release Notes and submit a PR. Tag Release on Github"
-echo "Press Y when you are done with these things and want to continue? [y/n]: "
+echo "Press any key (or y) when you are done writing the release notes and want to continue; press n otherwise."
+echo "Your response [y/n]: "
 read -r manualWork3
 case $manualWork3 in
-  n)
+  n|N)
     exit
-  ;;
-*)
-  ;;
+    ;;
+  *)
+    ;;
 esac
 #################### Part 5: Deploy to PyPI ################################
 cd "$BASE_DIR/.release/ternenv/tern" || (echo "cd Failed" && exit )
