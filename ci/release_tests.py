5a3615ad248df82e3326ab52ce9e25fb3a76cf3a
Alanna <aehill@umich.edu>
2022-04-14 16:37:35 +0000

    Made printed output more cohesive
    Remove the "START"/"END" strings from the
    print statements before and after
    each test to reduce clutter.
    
    Signed-off-by: Alanna Hill <aehill@umich.edu>

--- ci/release_tests.py
+++ ci/release_tests.py
@@ -47,11 +47,11 @@ tests = [
 ]
 
 # Run the tests
-print("BEGIN: Running test suite")
+print("START: Running test suite")
 for test in tests:
     # check = True will throw CalledProcessError for non-zero return code,
     # which is the same behavior as the deprecated .check_output
-    print(f"START: Runing test {test}")
+    print(f"START: Running test {test}")
     out = subprocess.run(test, shell=True, check=True, capture_output=True)  # nosec
     print(f"END: {test} passed successfully!")  # error not thrown - test passed
 
